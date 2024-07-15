a24c0880cc2b6e3fe39cb684b7e489d277219f15
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-07-15 20:46:39 +0600

    minimum CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,31 @@ import "./Navbar.css";
 import logo from "../assets/iutps-logo.png";
 
 const Navbar = () => (
-  <div id="nav">
+  <div
+    id="nav"
+    className="h-28 w-full px-15 fixed flex items-center justify-between md:justify-start md:px-28 z-50 tran"
+  >
     <a href="/">
-      <img src={logo} alt="IUTPS Logo" />
+      <img src={logo} alt="IUTPS Logo" className="h-12" />
     </a>
     <a href="/">
-      <div className="menu-items">
-        <h4>Home</h4>
+      <div className="mx-2.5 md:mx-5">
+        <h4 className="font-normal text-sm">HOME</h4>
       </div>
     </a>
     <a href="/">
-      <div className="menu-items">
-        <h4>Our Works</h4>
+      <div className="mx-2.5 md:mx-5">
+        <h4 className="font-normal text-sm">OUR WORKS</h4>
       </div>
     </a>
     <a href="#about-us">
-      <div className="menu-items">
-        <h4>About Us</h4>
+      <div className="mx-2.5 md:mx-5">
+        <h4 className="font-normal text-sm">ABOUT US</h4>
       </div>
     </a>
     <a href="#cards-holder">
-      <div className="menu-items">
-        <h4>Events</h4>
+      <div className="mx-2.5 md:mx-5">
+        <h4 className="font-normal text-sm">EVENTS</h4>
       </div>
     </a>
   </div>
